2e4bf46dccaa5ab567e1fde031663d8c625b7b1f
davidVacca <vakuzdg@gmail.com>
2024-05-12 04:43:36 -0500

    adding roles to aws-auth

--- eks-module/aws-auth.tf
+++ eks-module/aws-auth.tf
@@ -11,7 +11,7 @@ locals {
 
 # Update aws-auth configmap, to ensure additional role is added for Access Management in the K8s cluster
 # Terraform does not re-apply if command changes, therefore update null_resource name to force terraform to re-run this local exec
-resource "null_resource" "update_aws_auth10" {
+resource "null_resource" "update_aws_auth5" {
   depends_on = [aws_eks_cluster.k8scluster]
 
   provisioner "local-exec" {
@@ -28,17 +28,16 @@ resource "null_resource" "update_aws_auth10" {
               username: system:node:{{EC2PrivateDNSName}}
             - groups:
               - system:masters
-              rolearn: arn:aws:iam::838353990482:role/OrganizationAccountAccesRole
+              rolearn: arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/OrganizationAccountAccessRole
               username: adminRoleUser
             - groups:
               - system:masters
               rolearn: ${var.gitHubActionsAppCIrole}
-              username: gitHubActionsRoleUser 
+              username: GitHubActionsRoleUser
             - groups:
               - system:masters
               rolearn: ${var.gitHubActionsTerraformRole}
-              username: GitHubActionsTerraformRoleUser  
-          
+              username: GitHubActionsTerraformRoleUser
     EOF
     )"
     EOT
